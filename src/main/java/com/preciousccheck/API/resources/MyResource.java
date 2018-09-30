878c32dc187211cbea3d1aa3b7b509dbf5622930
Daniel Esponda <daniel.esponda@capitalone.com>
2018-09-30 11:21:17 -0400

    Setting project settings and configuration

---
+++ src/main/java/com/preciousccheck/API/resources/MyResource.java
@@ -0,0 +1,23 @@
+package com.preciousccheck.API.resources;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@RestController
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GetMapping("/myresource")
+    public String getIt() {
+        return "Got it!";
+    }
+}
